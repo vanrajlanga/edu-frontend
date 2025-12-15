ed7ceb7429ed631a72598a13d0c1af5762f103f7
krisngohil13 <krisngohil@gmail.com>
2025-12-15 22:29:43 +0530

    Add play icon to Icon component

--- src/components/ui/Icon.jsx
+++ src/components/ui/Icon.jsx
@@ -279,6 +279,13 @@ const icons = {
       d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
     />
   ),
+  play: (
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+    />
+  ),
 };
 
 function Icon({
